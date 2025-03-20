293d3d4c380655c56de8269845eec85b5daf7e6a
MD MUFID ALAM <117520484+Mdmufid@users.noreply.github.com>
2025-03-20 13:44:20 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -41,7 +41,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     }
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://e-comm-backend-r4td.onrender.com/images/${req.file.filename}`
     });
 });
 // creating for Creating products
