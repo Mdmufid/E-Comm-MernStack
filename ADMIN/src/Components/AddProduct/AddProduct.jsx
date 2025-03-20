a50fa32f6f5b48697477e612d3134bf438a97502
MD MUFID ALAM <117520484+Mdmufid@users.noreply.github.com>
2025-03-20 13:30:18 +0530

    Update AddProduct.jsx

--- ADMIN/src/Components/AddProduct/AddProduct.jsx
+++ ADMIN/src/Components/AddProduct/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
         
-        await fetch('http://127.0.0.1:4000/upload',{
+        await fetch('https://e-comm-backend-r4td.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json'
